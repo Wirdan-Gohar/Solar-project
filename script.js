1c7ef82321b6cb26a41460327728fe85e1258ca9
Wirdan Gohar <wirdan@99technologies.co>
2024-02-02 19:01:56 +0500

    first commit

---
+++ script.js
@@ -0,0 +1,7 @@
+$(document).ready(function () {
+    $('#panel-capacity').on('input change', function () {
+        $('#panel-capacity-value').text($(this).val() + ' Watts');
+    });
+
+    // Add more interactivity as needed
+});
